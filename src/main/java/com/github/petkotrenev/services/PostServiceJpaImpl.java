d529ab70687d5fcb3504b505a2969984d3c04e83
Petko <petkotrenev@gmail.com>
2018-06-11 19:12:05 +0300

    Add Web Security Config Add concurrent edit of a post by users Add Edit post html page Clean up pathing for asserts

--- src/main/java/com/github/petkotrenev/services/PostServiceJpaImpl.java
+++ src/main/java/com/github/petkotrenev/services/PostServiceJpaImpl.java
@@ -7,12 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.util.List;
+
+import static java.time.Instant.now;
 
 @Service
 public class PostServiceJpaImpl implements PostService{
 
+    public static final int LOCK_DURATION = 10;
     @Autowired
     PostRepository postRepository;
 
@@ -23,6 +27,7 @@ public class PostServiceJpaImpl implements PostService{
 
     @Override
     public List<Post> findLatest5() {
+
         return postRepository.findTop5OrderByCreationDate(new PageRequest(0,5));
     }
 
@@ -32,23 +37,50 @@ public class PostServiceJpaImpl implements PostService{
     }
 
     @Override
-    public void create(Post post) { postRepository.save(post);
+    public Post create(Post post) {
+        return postRepository.save(post);
     }
 
     @Override
-    public void create(Post post, User user) {
+    public Post create(Post post, User user) {
         post.setCreator(user);
         post.setCreationDate(LocalDate.now());
-        create(post);
+        return create(post);
     }
 
     @Override
-    public void edit(Post post) {
-        postRepository.save(post);
+    public Post update(Post post) {
+        return postRepository.save(post);
     }
 
     @Override
     public void deleteById(Long id) {
         postRepository.delete(id);
+    }
+
+    @Override
+    public Post lockForUser(Post post, User user) {
+        post.setLastLockedBy(user);
+        post.setLockTimestamp(now()); // ms after era
+        return postRepository.save(post);
+    }
+
+    @Override
+    public Post unlock(Post post) {
+        post.setLastLockedBy(null);
+        post.setLockTimestamp(null); // ms after era
+        return postRepository.save(post);
+    }
+
+    @Override
+    public boolean isPostLockedForUser(Post post, User user) {
+        User lockedBy = post.getLastLockedBy();
+
+        if (lockedBy == null) return false;
+        if (lockedBy.getId().equals(user.getId())) return false;
+
+        Duration duration = Duration.between(post.getLockTimestamp(), now());
+
+        return duration.toMinutes() < LOCK_DURATION;
     }
 }
